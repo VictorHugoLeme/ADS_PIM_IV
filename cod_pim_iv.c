0af6e8d5f7eeb0dcd208214fba856aa11a4cdf42
Victor Hugo Leme <victor.hugom.leme@gmail.com>
2021-11-05 21:33:06 -0300

    mudando esquema de salvamento

--- cod_pim_iv.c
+++ cod_pim_iv.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
 //structs
 
 typedef struct
 {
     char nome[50];
-    int idade[50];
-    int cpf[50];
-    char telefone[50]; 
+    int idade[3];
+    char cpf[20];
+    char telefone[20]; 
     char cep[50];
     char endereco[150];
     char data_nascimento[50];
     char email[50];
     char comor[200];
     char data_diag[50];
+		int bool_comorb[1];
 }paciente;
 
 paciente lista[10];
-int tam = 10;
+int tam = 11;
 int qt = 0;
 
 
@@ -28,10 +29,7 @@ int qt = 0;
 void login();
 int cadastro();
 void imprimir();
-void salvarArquivo();
-
-
-
+void salvarArquivoComorb();
 
 //main
 int main()
@@ -44,8 +42,7 @@ int main()
 
     do
     {
-        printf("\n\n\n3 - Cadastrar\n");
-        printf("2 - Salvar em Arquivo\n");
+        printf("\n\n\n2 - Cadastrar\n");
         printf("1 - Imprimir\n");
         printf("0 - Sair\n");
         scanf("%d", &op);
@@ -59,21 +56,14 @@ int main()
             imprimir();
             break;
         case 2:
-            salvarArquivo();
-            break;
-        case 3:
             cadastro();
             break;
 
-
-        
         default:
             printf("Opcao Invalida");
             break;
         }
     }while (op != 0);
-    
-    
     
     return 0;
 }
@@ -83,33 +73,33 @@ void login(){
 			char username[15]; 
 			char password[12]; 
 			
-		while (1) {
-			printf("Nome de usuario:\n"); 
-			scanf("%s",&username);
+			while (1) {
+				printf("Nome de usuário:\n"); 
+				scanf("%s",&username);
 
-			if(strcmp(username,"medico")!=0){
-				printf("Nome de usuario invalido.Por favor, tente novamente:\n");
-				continue;
-			}
-			else{
-				break;
+				if(strcmp(username,"medico")!=0){
+					printf("Nome de usuário inválido.Por favor, tente novamente:\n");
+					continue;
+				}
+				else{
+					break;
 				}
 			}
     
 		while (1) {
 			printf("Entre sua senha:\n"); 
-            scanf("%s",&password); 
+      scanf("%s",&password); 
 
-            if(strcmp(password,"1111")!=0){ 
-				printf("Senha invalida.Por favor, tente novamente:\n");
+      if(strcmp(password,"1111")!=0){ 
+				printf("Senha inválida.Por favor, tente novamente:\n");
 
-				continue;
+					continue;
 			}
 			else{
 				printf("\nWelcome.Login Success!");
 				break;
 			}
-        }
+    }
 		return;
 	}
 
@@ -122,53 +112,98 @@ void login(){
             system("cls");
             paciente p;
             scanf("%*c");
-            printf("\t\tDigite o Nome do Paciente: \n");
+            printf("\tDigite o Nome do Paciente: \n");
             fflush(stdin);
             fgets(p.nome, 50, stdin);
 
             fflush(stdin);
-            printf("\t\tDigite a Idade do Paciente: \n");
-            scanf("%d", p.idade);
+            printf("\tDigite a Idade do Paciente: \n");
+            fgets(p.idade, 3, stdin);
 
             fflush(stdin);
-            printf("\t\tDigite o CPF do Paciente: \n");
+            printf("\tDigite o CPF do Paciente: \n");
             fgets(p.cpf, 20, stdin);
+						getchar();
 
             fflush(stdin);
-            printf("\t\tDigite o telefone do Paciente: \n");
+            printf("\tDigite o telefone do Paciente: \n");
             fgets(p.telefone, 20, stdin);
+						getchar();
 
             fflush(stdin);
-            printf("\t\tDigite o CEP: \n");
+            printf("\tDigite o CEP: \n");
             fgets(p.cep, 50, stdin);
+						getchar();
 
             fflush(stdin);
-            printf("\t\tDigite o Endereco completo(Rua, Numero, bairro, Cidade e Estado): \n");
+            printf("\tDigite o Endereco completo(Rua, Numero, bairro, Cidade e Estado): \n");
             fgets(p.endereco, 150, stdin);
+						getchar();
             
             fflush(stdin);
-            printf("\t\tDigite a data de nascimento(xx/xx/xxxx): \n");
+            printf("\tDigite a data de nascimento(xx/xx/xxxx): \n");
             fgets(p.data_nascimento, 30, stdin);
+						getchar();
 
-            printf("\t\tDigite o Email do Pacinte: \n");
+						fflush(stdin);
+            printf("\tDigite o Email do Paciente: \n");
             fgets(p.email, 50, stdin);
+						getchar();
 
-            printf("\t\tEscreva Patologias do Paciente(diabetes, obesidade, hipertensao, tuberculose, outros): \n");
-            fgets(p.comor, 200, stdin);
+						fflush(stdin);
+						printf("\tO Paciente possui alguma comorbidade?\n1-Sim\n2-Nao\n");
+						scanf("%d", p.bool_comorb, 1, stdin);
+						getchar();
 
-            printf("\t\tDigite a data do Diagnostico: \n");
+						if (p.bool_comorb != 0) {
+							fflush(stdin);
+							printf("\tEscreva Patologias do Paciente(diabetes, obesidade, hipertensão, tuberculose, outros): \n");
+            	fgets(p.comor, 200, stdin);
+						}		
+						
+						fflush(stdin);
+            printf("\tDigite a data do Diagnostico: \n");
             fgets(p.data_diag, 30, stdin);
 
-            
+						if (p.idade > 65 || p.comor == 1) {
+							// paciente risco, salvar nos dois arquivos
+							FILE *arq = fopen("PacienteRisco.txt", "w");
+        			int i;
+        			if(arq)
+        			{
+           			for(i = 0; i < qt; i++)
+           				{
+            			fprintf(arq, "%s", lista[i].nome);
+            			fprintf(arq, "%d\n", lista[i].idade);
+            			fprintf(arq, "%s\n", lista[i].cep);
+            			}
+            		fclose(arq);
+								}
+							
+							// paciente normal, salvar apenas em pacientes
+							FILE *arq2 = fopen("pacientes.txt", "w");
 
+        			if(arq2)
+        			{
+           			for(i = 0; i < qt; i++)
+           				{
+            			fprintf(arq2, "%s", lista[i].nome);
+            			fprintf(arq2, "%d\n", lista[i].idade);
+            			fprintf(arq2, "%s\n", lista[i].cep);
+            			}
+            		fclose(arq2);
+        			}
+						}
 
             lista[qt] = p;
             qt++;
             return 1;
             
-        }else{
-            printf("Error");
-            return 0;
+        }
+				else
+				{
+        	printf("Error");
+        	return 0;
         }
 
 
@@ -187,24 +222,4 @@ void login(){
         printf("CEP: %s\n", lista[i].cep);
         }
 
-    }
-
-
-
-    void salvarArquivo()
-    {
-        FILE *arq = fopen("paciente.txt", "w");
-        int i;
-        if(arq)
-        {
-            for(i = 0; i < qt; i++)
-            {
-            fprintf(arq, "%s", lista[i].nome);
-            fprintf(arq, "%d\n", lista[i].idade);
-            fprintf(arq, "%s\n", lista[i].cep);
-            }
-            fclose(arq);
-        }
-        else
-        printf("error.");
     }
